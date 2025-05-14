3009c63f5e30a55df5f7e50ba45c87627ba14ab0
Mr-Shehroz <mr.shehroz.khan2006>
2025-05-14 14:25:30 +0500

    add

--- src/app/components/profession-sec1.tsx
+++ src/app/components/profession-sec1.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Section1() {
     return (
@@ -14,14 +16,19 @@ export default function Section1() {
                 />
 
                 {/* Circle */}
-                <svg
+                <motion.svg
                     className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[80%] w-[80%]"
                     fill="none"
                     stroke="white"
+                    strokeWidth={4}
                     xmlns="http://www.w3.org/2000/svg"
                 >
-                    <circle cx="50%" cy="50%" r="40%" />
-                </svg>
+                    <motion.circle
+                        initial={{ opacity: 0, r: '45%', rotateZ: 0 }}
+                        animate={{ opacity: 1, rotateZ: 360, r: '40%' }}
+                        transition={{ duration: 1.5 }}
+                        cx="50%" cy="50%" r="40%" />
+                </motion.svg>
 
                 {/* Desktop Buttons */}
                 <div className="absolute hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:flex flex-col gap-3 items-center px-4">
@@ -38,7 +45,7 @@ export default function Section1() {
             </div>
 
             {/* Right: Text section */}
-            <div className="bg-[#a6c3d1] w-full h-[80dvh] md:h-screen flex flex-col justify-center items-center text-black px-6 md:px-8 md:py-0">
+            <div className="bg-[#a6c3d1] w-[70%] h-[80dvh] md:h-screen flex flex-col justify-center items-center text-black px-6 md:px-8 md:py-0">
                 <div className="text-center z-50 space-y-2">
                     <h2 className="text-4xl md:text-7xl font-bold">SUPER</h2>
                     <h2 className="text-4xl md:text-7xl font-bold">QUALIFIED</h2>
--- src/app/components/profession-sec6.tsx
+++ src/app/components/profession-sec6.tsx
@@ -9,6 +9,7 @@ export default function Section6() {
                         className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[80%] w-[80%]"
                         fill="none"
                         stroke="white"
+                        strokeWidth={4}
                         xmlns="http://www.w3.org/2000/svg"
                     >
                         <circle cx="50%" cy="50%" r="50%" />
@@ -16,7 +17,7 @@ export default function Section6() {
 
                     {/* Centered Text & Button */}
                     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4 text-center">
-                        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black">
+                        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white">
                             COMPANIES
                         </h2>
                         <button className="bg-white text-black px-6 py-2 sm:px-8 sm:py-3 rounded-full font-medium text-sm sm:text-base md:text-lg lg:text-xl hover:bg-gray-200 transition">
